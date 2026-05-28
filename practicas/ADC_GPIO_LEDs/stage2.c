108f98c53a7dce3a030c32b8a498ccb447fc7463
GregorioOrozco <A01641967@tec.mx>
2026-05-28 15:30:22 -0600

    Update stage2.c

--- practicas/ADC_GPIO_LEDs/stage2.c
+++ practicas/ADC_GPIO_LEDs/stage2.c
@@ -11,33 +11,35 @@
 #include "fsl_gpio.h"
 #include "fsl_adc16.h"
 
-//variables globales
+// Tipos de datos
 typedef enum
 {
 	SENSOR_LIGHT,
 	SENSOR_TEMP,
 	SENSOR_BUTTON
 } sensor_type_t;
+
 typedef struct
 {
 	sensor_type_t type;
 	uint16_t value;
 } sensor_msg_t;
 
-//definimos
+// Definiciones de Hardware
 #define BUTTON_PORT GPIOB
 #define BUTTON_PIN 0U /* Assigned to PTB0 (J10 - pin 2 on the board)*/
 #define ADC_BASE ADC0
 #define ADC_CH_LIGHT 9U //PTB1
 #define ADC_CH_TEMPERATURE 12U //PTB2
 
-//example thresholds
+// Thresholds de ejemplo
 #define LIGHT_THRESHOLD 2048
 #define TEMP_THRESHOLD 2048
 
+// Handlers globales válidos
 QueueHandle_t sensorQueue;
 
-//prototypes
+// Prototipos de funciones (Corregidos)
 static void vTaskLightSensor(void *pvParameters);
 static void vTaskTemperatureSensor(void *pvParameters);
 static void vTaskButtonPolling(void *pvParameters);
@@ -57,6 +59,7 @@ int main(void) {
     ADC16_EnableHardwareTrigger(ADC_BASE, false);
     ADC16_DoAutoCalibration(ADC_BASE);
 
+    // Creación de la Cola para 10 mensajes
     sensorQueue = xQueueCreate(10, sizeof(sensor_msg_t));
 
     if(sensorQueue == NULL)
@@ -65,87 +68,98 @@ int main(void) {
     	while(1);
     }
 
-    PRINTF("FreeRTOS KL25z - Tasks with sensors without queues or interruptions\r\n");
+    PRINTF("FreeRTOS KL25z - Stage 2: Queue mechanics active\r\n");
 
-    // Definición de tareas
-    xTaskCreate(vTaskBoton, "Light", configMINIMAL_STACK_SIZE + 100,
-    		NULL,
-    		2,
-    		NULL);
-    // Inciamos las tareas
+    // Creación de tareas (Nombres de funciones corregidos aquí)
+    xTaskCreate(vTaskLightSensor, 
+    			"Light", 
+    			configMINIMAL_STACK_SIZE + 100,
+    			NULL,
+    			2,
+    			NULL);
+
     xTaskCreate(vTaskTemperatureSensor,
-    		"Temp",
-			configMINIMAL_STACK_SIZE + 100,
-			NULL,
-			2,
-			NULL);
+    			"Temp",
+				configMINIMAL_STACK_SIZE + 100,
+				NULL,
+				2,
+				NULL);
+
     xTaskCreate(vTaskButtonPolling,
-    		"Button",
-			configMINIMAL_STACK_SIZE + 100,
-			NULL,
-			1,
-			NULL);
+    			"Button",
+				configMINIMAL_STACK_SIZE + 100,
+				NULL,
+				1,
+				NULL);
+
     xTaskCreate(vTaskLedControl,
-    		"LEDs",
-			configMINIMAL_STACK_SIZE + 100,
-			NULL,
-			3,
-			NULL);
+    			"LEDs",
+				configMINIMAL_STACK_SIZE + 100,
+				NULL,
+				3,
+				NULL);
+
     vTaskStartScheduler();
     while(1){}
 }
 
 static void vTaskLightSensor(void *pvParameters)
 {
+	sensor_msg_t msg; // <-- Corregido: Agregada variable local faltante
 	adc16_channel_config_t adcConfigLight;
 	adcConfigLight.channelNumber = ADC_CH_LIGHT;
 	adcConfigLight.enableInterruptOnConversionCompleted = false;
 	adcConfigLight.enableDifferentialConversion = false;
+	
 	while(1)
 	{
 		/* Trigger conversion */
 		ADC16_SetChannelConfig(ADC_BASE, 0U, &adcConfigLight);
 		/* Wait for conversion to end */
 		while (0U == (kADC16_ChannelConversionDoneFlag & ADC16_GetChannelStatusFlags(ADC_BASE, 0U))) {}
+		
 		/* Read result */
 		msg.type = SENSOR_LIGHT;
-		/* Read result */
-		msg.value = ADC16_GetChannelConversionValue(ADC_BASE, 0U);;
+		msg.value = ADC16_GetChannelConversionValue(ADC_BASE, 0U);
+		
 		xQueueSend(sensorQueue, &msg, pdMS_TO_TICKS(10));
 		vTaskDelay(pdMS_TO_TICKS(500));
 	}
 }
 
-static void TaskTemperatureSensor(void *pvParameters){
+static void vTaskTemperatureSensor(void *pvParameters) // <-- Corregido: Nombre de la función alineado con el prototipo
+{
 	sensor_msg_t msg;
 	adc16_channel_config_t adcConfigTemperature;
 	adcConfigTemperature.channelNumber = ADC_CH_TEMPERATURE;
 	adcConfigTemperature.enableInterruptOnConversionCompleted = false;
 	adcConfigTemperature.enableDifferentialConversion = false;
+	
 	while(1){
 		ADC16_SetChannelConfig(ADC_BASE, 0U, &adcConfigTemperature);
 		while (0U == (kADC16_ChannelConversionDoneFlag & ADC16_GetChannelStatusFlags(ADC_BASE, 0U))) {}
+		
 		msg.type = SENSOR_TEMP;
-		/* Read result */
 		msg.value = ADC16_GetChannelConversionValue(ADC_BASE, 0U);
+		
 		xQueueSend(sensorQueue, &msg, pdMS_TO_TICKS(10));
 		vTaskDelay(pdMS_TO_TICKS(500));
 	}
-
 }
 
 static void vTaskButtonPolling(void *pvParameters)
 {
 	sensor_msg_t msg;
 	uint32_t button_state = 1;
-	uint32_t last_state = 1; /* Physical or internal Pull-Up configuration assumed */
+	uint32_t last_state = 1; 
+	
 	for (;;) {
 		button_state = GPIO_ReadPinInput(BUTTON_PORT, BUTTON_PIN);
 		/* Falling edge detection */
 		if (button_state == 0 && last_state == 1) {
-			// PRINTF("Event: Button pressed.\r\n");
 			last_state = button_state;
 		}
+		
 		msg.type = SENSOR_BUTTON;
 		msg.value = button_state;
 		xQueueSend(sensorQueue, &msg, pdMS_TO_TICKS(10));
@@ -161,31 +175,35 @@ static void vTaskLedControl(void *pvParameters)
 	uint16_t light_value = 0;
 	uint16_t temp_value = 0;
 	uint16_t button_value = 0;
+	
 	while(1)
 	{
-		if(xQueueReceive(sensorQueue, &msg, portMAX_DELAY) ==
-		pdPASS)
+		// Bloqueo eficiente hasta recibir un mensaje de la cola
+		if(xQueueReceive(sensorQueue, &msg, portMAX_DELAY) == pdPASS)
 		{
 			switch(msg.type)
 			{
-			case SENSOR_LIGHT:
-				light_value = msg.value;
-				break;
-			case SENSOR_TEMP:
-				temp_value = msg.value;
-				break;
-			case SENSOR_BUTTON:
-				button_value = msg.value;
-				break;
+				case SENSOR_LIGHT:
+					light_value = msg.value;
+					break;
+				case SENSOR_TEMP:
+					temp_value = msg.value;
+					break;
+				case SENSOR_BUTTON:
+					button_value = msg.value;
+					break;
 			}
+			
 			if(light_value < LIGHT_THRESHOLD)
 				LED_BLUE_ON();
 			else
 				LED_BLUE_OFF();
+				
 			if(temp_value > TEMP_THRESHOLD)
 				LED_RED_ON();
 			else
 				LED_RED_OFF();
+				
 			if(!button_value)
 				LED_GREEN_ON();
 			else
