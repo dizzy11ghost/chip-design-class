4f83b9e45a75e7e77153989c25142baf0935fb04
Lego Leñero <82170264+dizzy11ghost@users.noreply.github.com>
2026-06-08 20:24:22 -0600

    Adjust yellow color range for detection

--- DOBORT/rasp/main_rasp.py
+++ DOBORT/rasp/main_rasp.py
@@ -73,7 +73,7 @@ cap = cv2.VideoCapture(0)
 lower_red1   = np.array([0,   100, 100]); upper_red1   = np.array([10,  255, 255])
 lower_red2   = np.array([170, 100, 100]); upper_red2   = np.array([180, 255, 255])
 lower_blue   = np.array([100, 100, 100]); upper_blue   = np.array([140, 255, 255])
-lower_yellow = np.array([10,  100, 100]); upper_yellow = np.array([25,  255, 255])
+lower_yellow = np.array([24,  100, 100]); upper_yellow = np.array([25,  255, 255])
 
 # ── Estados ───────────────────────────────────────────────────────────────────
 IDLE         = "IDLE"
