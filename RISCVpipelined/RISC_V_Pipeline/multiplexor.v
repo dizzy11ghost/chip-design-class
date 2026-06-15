f6a051def034df2a578bc52a576fab936d251390
Lego Leñero <82170264+dizzy11ghost@users.noreply.github.com>
2026-06-14 23:04:02 -0600

    Delete RISCVpipelined/RISC_V_Pipeline directory

--- RISCVpipelined/RISC_V_Pipeline/multiplexor.v
+++
@@ -1,9 +0,0 @@
-module multiplexor #(parameter N = 2) (
-    input  [N*32-1:0]        mux_in,
-    input  [$clog2(N)-1:0]   mux_sel,
-    output reg [31:0]        mux_out
-);
-    always @(*)
-        mux_out = mux_in[(mux_sel*32) +: 32]; 
-
-endmodule
--- RISCVpipelined/RISC_V_Pipeline/sumador.v
+++
@@ -1,9 +0,0 @@
-module sumador(
-	input [31:0] A,
-	input [31:0] B,
-	output [31:0] out
-);
-	
-	assign out = A + B; 
-
-endmodule 
